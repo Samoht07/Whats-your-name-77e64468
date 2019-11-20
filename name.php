4b98c03e97cc2f7e4b7d37f15781359999773e34
Samoht07 <341737@talnet.nl>
2019-11-20 10:51:22 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,8 @@
+<?php
+echo "Wat is je voornaam?" . PHP_EOL;
+$voor = readline(">");
+echo "Wat is je achternaam?"  . PHP_EOL;
+$achter = readline(">");
+$spat = " ";
+echo 'Je naam is: '.$voor .$spat .$achter;
+?>
